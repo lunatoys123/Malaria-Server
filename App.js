03ffb6302142a6797f8303ca567e4e9e506b012e
laukintung322@gmail.com <laukintung322@gmail.com>
2022-09-03 09:35:30 +0800

    implement jsonwebtoken and errorHandler

--- App.js
+++ App.js
@@ -6,11 +6,17 @@ import cors from "cors";
 import HospitalRouter from "./Router/Hospital_Router.js";
 import userRouter from "./Router/User_Router.js";
 
+import authJWT from "./helpers/jwt.js";
+import errorHandler from "./helpers/ErrorHandler.js";
+
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use(cors());
 app.options("*", cors());
+
+app.use(authJWT());
+app.use(errorHandler);
 
 app.use("/Malaria/Hospital", HospitalRouter);
 app.use("/Malaria/User", userRouter);
