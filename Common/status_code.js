4a9649d2dc3dfbaabefd26ce5ecdcc9197581534
Tony Lau <tony.lau@ifingate.com>
2022-09-09 17:55:39 +0800

    update on status code and WHO controller

--- Common/status_code.js
+++ Common/status_code.js
@@ -2,3 +2,18 @@ export const status_code = {
   Failed: "Failed",
   Success: "Success",
 };
+
+export const WHO_Indicator_code = {
+  "MALARIA_PF_INDIG": "Number of indigenous P. falciparum malaria cases",
+  "MALARIA_IMPORTED": "Number of imported malaria cases",
+  "MALARIA_EST_CASES": "Estimated number of malaria cases",
+  "MALARIA_SUSPECTS": "Number of suspected malaria cases",
+  "MALARIA_INDIG_STATUS": "Status of indigenous malaria cases",
+  "MALARIA_PV_INDIG": "Number of indigenous P. vivax malaria cases",
+  "MALARIA_INDIG": "Number of indigenous malaria cases",
+  "MALARIA_CONF_CASES": "Number of confirmed malaria cases",
+  "MALARIA_ACT_TREATED": "Number of malaria cases treated with artemisinin-based combination therapies (ACTs)",
+  "MALARIA_1STLINE_TREATED": "Number of malaria cases treated with any first  line tx courses (including artemisinin-based combination therapies (ACTs)",
+  "MALARIA_PRES_CASES": "Number of presumed malaria cases",
+  "MALARIA_TOTAL_CASES": "Total number of  malaria cases (presumed + confirmed cases)"
+}
--- Controller/WHO_controller.js
+++ Controller/WHO_controller.js
@@ -1,7 +1,19 @@
-import axios from "axios";
+import mongoose from "mongoose";
+import Malaria from "../Model/Malaria.js";
+import { WHO_Indicator_code, status_code } from "../Common/status_code.js";
+
+const WHO = Malaria.WHO;
+
 export const Preview = async (req, res) => {
-  const response = await axios.get(
-    "https://ghoapi.azureedge.net/api/Indicator?$filter=contains(IndicatorName,'malaria case')"
-  );
-  console.log(response.data);
+
+  var Indication_code = await WHO.distinct("Indication_code");
+  var keys = Indication_code.map((code) => {
+    return WHO_Indicator_code[code];
+  })
+
+  if (keys == null || keys.length == 0) {
+    return res.status(404).send({ status: status_code.Failed, Error: "WHO Indicator code is not working" });
+  }
+
+  return res.status(200).send({ status: status_code.Success, data: keys });
 };
