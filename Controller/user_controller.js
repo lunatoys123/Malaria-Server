01afdae660575ab1a41895e6da7c73aa444a57e0
Lau Kin Tung <laukintung322@gmail.com>
2023-01-04 11:51:10 +0800

    update on Register user

--- Controller/user_controller.js
+++ Controller/user_controller.js
@@ -8,96 +8,85 @@ const Doctor = Malaria.Doctor;
 const Hospital = Malaria.Hospital;
 
 export const Register = async (req, res) => {
-  const request = req.body;
-  const Login_name = request.Login_name;
-  const Password = bcrypt.hashSync(request.Password, 10);
-  const Phone_number = request.Phone_Number;
-  const Email = request.email;
-  const Hospital_id = mongoose.Types.ObjectId(request.Hospital_id);
+	const request = req.body;
+	const Login_name = request.Login_name;
+	const Role = request.Role;
+	const Password = bcrypt.hashSync(request.Password, 10);
+	const Phone_number = request.Phone_Number;
+	const Email = request.email;
+	const Hospital_id = mongoose.Types.ObjectId(request.Hospital_id);
 
-  const exist_hospital_id = await Hospital.findOne({ _id: Hospital_id });
-  if (exist_hospital_id == null) {
-    return res
-      .status(400)
-      .send({ status: status_code.Failed, Message: "Hospital id not exist" });
-  }
+	const exist_hospital_id = await Hospital.findOne({ _id: Hospital_id });
+	if (exist_hospital_id == null) {
+		return res.status(400).send({ status: status_code.Failed, Message: "Hospital id not exist" });
+	}
 
-  const newUser = new Doctor({
-    Login_name,
-    Password,
-    Phone_number,
-    Email,
-    Hospital_id,
-  });
+	const newUser = new Doctor({
+		Login_name,
+    Role,
+		Password,
+		Phone_number,
+		Email,
+		Hospital_id,
+	});
 
-  const user = await Doctor.findOne({ Email: Email });
+	const user = await Doctor.findOne({ Email: Email });
 
-  if (user) {
-    return res
-      .status(400)
-      .send({ status: status_code.Failed, Message: "User exists already" });
-  }
+	if (user) {
+		return res.status(400).send({ status: status_code.Failed, Message: "User exists already" });
+	}
 
-  await newUser
-    .save()
-    .then((data) => {
-      return res.status(200).send({
-        status: status_code.Success,
-        Message: "Add user successfully",
-      });
-    })
-    .catch((err) => {
-      return res
-        .status(400)
-        .send({ status: status_code.Failed, error: err.message });
-    });
+	await newUser
+		.save()
+		.then(data => {
+			return res.status(200).send({
+				status: status_code.Success,
+				Message: "Add user successfully",
+			});
+		})
+		.catch(err => {
+			return res.status(400).send({ status: status_code.Failed, error: err.message });
+		});
 };
 
 export const Login = async (req, res) => {
-  const request = req.body;
-  const Email = request.email;
-  const Password = request.Password;
+	const request = req.body;
+	const Email = request.email;
+	const Password = request.Password;
 
-  const user = await Doctor.findOne({ Email: Email });
-  if (!user) {
-    return res
-      .status(400)
-      .send({ status: status_code.Failed, Message: "User Not exist" });
-  }
-  if (user && bcrypt.compareSync(Password, user.Password)) {
-    const token = jwt.sign(
-      {
-        login_name: user.Login_name,
-        Doctor_id: user._id
-      },
-      "Malaria",
-      { expiresIn: "1d" }
-    );
-    return res
-      .status(200)
-      .send({
-        status: status_code.Success,
-        Message: "Login successful",
-        token,
-      });
-  } else {
-    return res
-      .status(404)
-      .send({ status: status_code.Failed, Message: "Wrong Password" });
-  }
+	const user = await Doctor.findOne({ Email: Email });
+	if (!user) {
+		return res.status(400).send({ status: status_code.Failed, Message: "User Not exist" });
+	}
+	if (user && bcrypt.compareSync(Password, user.Password)) {
+		const token = jwt.sign(
+			{
+				login_name: user.Login_name,
+				Doctor_id: user._id,
+        role: user.Role
+			},
+			"Malaria",
+			{ expiresIn: "1d" }
+		);
+		return res.status(200).send({
+			status: status_code.Success,
+			Message: "Login successful",
+			token,
+		});
+	} else {
+		return res.status(404).send({ status: status_code.Failed, Message: "Wrong Password" });
+	}
 };
 
 export const GetAllUser = async (req, res) => {
-  const All_User = await Doctor.find({}, { Login_name: 1, _id: 0 });
+	const All_User = await Doctor.find({}, { Login_name: 1, _id: 0 });
 
-  if (!All_User) {
-    return res.status(500).send({
-      status: status_code.Failed,
-      Message: "Server Error, no user display",
-    });
-  }
+	if (!All_User) {
+		return res.status(500).send({
+			status: status_code.Failed,
+			Message: "Server Error, no user display",
+		});
+	}
 
-  return res
-    .status(200)
-    .send({ status: status_code.Success, user_list: All_User });
+	return res.status(200).send({ status: status_code.Success, user_list: All_User });
 };
